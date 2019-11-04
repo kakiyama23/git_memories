c7e1cabad48d8fd5ef5e988f33a9d2ba73080089
EC2 Default User <ec2-user@ip-172-31-34-32.ap-northeast-1.compute.internal>
2019-11-04 07:36:55 +0000

    first commit

---
+++ app/assets/javascripts/tops.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#calendar").fullCalendar({});
+  
+  $('#welcome-btn').click(function() {
+    $('#welcome-btn, #welcome-wrapper').fadeOut(200);
+    $('.modal-wrapper').fadeIn(500);
+  });
+  
+  $('#read-more').click(function() {
+    $('.important').css('color', 'red');
+    $('#more-text').slideDown();
+    $('#read-more').hide();
+    $('#image-wrapper').slideUp();
+  });
+  
+  $('#start').click(function() {
+    $('#description-wrapper').hide();
+    $('#start-wrapper').fadeIn(1500);
+  });
+});
